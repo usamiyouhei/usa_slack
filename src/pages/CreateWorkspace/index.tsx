f94b03a8fdc1200fd855b92b7892845be2d2fcc6
youhei usami <usao9110@icloud.com>
2026-04-23 18:56:42 +0400

    4/23

--- src/pages/CreateWorkspace/index.tsx
+++ src/pages/CreateWorkspace/index.tsx
@@ -2,14 +2,24 @@ import "../Signup/auth.css";
 import CreateWorkspaceModal from "../Home/WorkspaceSelector/CreateWorkspaceModal";
 import { useCurrentUserStore } from "../../modules/auth/current-user.state";
 import { Navigate } from "react-router-dom";
+import { workspaceRepository } from "../../modules/workspaces/workspace.repository";
 function CreateWorkspace() {
   const { currentUser } = useCurrentUserStore();
 
   if (currentUser == null) return <Navigate to="/signin" />;
 
+  const createWorkspace = async (name: string) => {
+    try {
+      const newWorkspace = await workspaceRepository.create(name);
+      console.log(newWorkspace);
+    } catch (error) {
+      console.log("ワークスペースの作成に失敗しました", error);
+    }
+  };
+
   return (
     <div>
-      <CreateWorkspaceModal />
+      <CreateWorkspaceModal onSubmit={createWorkspace} />
     </div>
   );
 }
