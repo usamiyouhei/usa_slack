90721679f1ba7b2a13d34ef1756999f60bfba0da
youhei usami <usao9110@icloud.com>
2026-04-26 09:34:09 +0400

    4/26

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,33 @@
-import WorkspaceSelector from './WorkspaceSelector';
-import './Home.css';
-import Sidebar from './Sidebar';
-import MainContent from './MainContent';
+import WorkspaceSelector from "./WorkspaceSelector";
+import "./Home.css";
+import Sidebar from "./Sidebar";
+import MainContent from "./MainContent";
+import { useCurrentUserStore } from "../../modules/auth/current-user.state";
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { Workspace } from "../../modules/workspaces/workspace.entity";
+import { workspaceRepository } from "../../modules/workspaces/workspace.repository";
 
+interface Props {
+  workspaces: any[];
+}
 function Home() {
+  const { currentUser } = useCurrentUserStore();
+  const [workspaces, setWorkspaces] = useState<Workspace[]>();
+
+  const fetchWorkspaces = async () => {
+    try {
+      const workspaces = await workspaceRepository.find();
+      setWorkspaces(workspaces);
+    } catch (error) {
+      console.error("ワークスペースの取得に失敗しました", error);
+    }
+  };
+  useEffect(() => {
+    fetchWorkspaces();
+  }, []);
+
+  if (currentUser == null) return <Navigate to="/signin" />;
   return (
     <div className="slack-container">
       <WorkspaceSelector />
